06dfb6ad0de55717a1b3273eaaa6825b01a5f99f
MEBonham <programmer@mckaybonham.com>
2020-04-17 05:39:44 -0600

    Minor header work

--- cre8/src/components/header/Header.js
+++ cre8/src/components/header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
             <div className="banner-contents">
-                <h1>CRE8 Alpha</h1>
+                <Link to="/"><h1>CRE8 Alpha</h1></Link>
             </div>
             <div className="dummy-space" />
         </header>
