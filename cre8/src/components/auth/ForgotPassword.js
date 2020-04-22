8cbe25c6461a22d24e4b3f33bda2b601ed29cf30
MEBonham <programmer@mckaybonham.com>
2020-04-22 11:07:59 -0600

    ForgotPassword also Redirects properly

--- cre8/src/components/auth/ForgotPassword.js
+++ cre8/src/components/auth/ForgotPassword.js
@@ -1,9 +1,10 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Store } from '../GlobalWrapper';
 import fb from '../../fbConfig';
 
 import useForm from '../../hooks/useForm';
+import LoadingAlert from '../other/LoadingAlert';
 
 const ForgotPassword = () => {
     const [state] = useContext(Store);
@@ -21,8 +22,9 @@ const ForgotPassword = () => {
     }
     const { inputs, handleInputChange, handleSubmit } = useForm(resetPw);
 
-    const component = state.user ? 
-        <Redirect to="/" /> :
+    const [component, setComponent] = useState(<LoadingAlert />);
+    const firstLoad = useRef(true);
+    const [formCode] = useState(
         <form onSubmit={handleSubmit} className="primary-content content-padding login-form rows">
             <h1>Forgot Password</h1>
             <div>
@@ -38,9 +40,16 @@ const ForgotPassword = () => {
             <button type="submit">Reset Password</button>
             {errorMessage ? <p className="buffer-above error-message">{errorMessage}</p> : null}
             {confirmMessage ? <p className="buffer-above">{confirmMessage}</p> : null}
-        </form>;
-
-    return component;
-}
+        </form>
+    );
+    useEffect(() => {
+        if (firstLoad.current) {
+            firstLoad.current = false;
+        } else {
+            setComponent(state.user ? <Redirect to="/" /> : formCode);
+        }
+    }, [formCode, state.user])
+    return (component);
+    }
 
 export default ForgotPassword;
