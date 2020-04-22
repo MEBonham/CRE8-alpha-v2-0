4d8868ce1fc8890b6d1220f5553d24686c98a371
MEBonham <programmer@mckaybonham.com>
2020-04-21 21:20:00 -0600

    FOUND BUG CULPRIT

--- cre8/src/components/header/SignedOut.js
+++ cre8/src/components/header/SignedOut.js
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import profileButton from '../../media/profile-icon.png';
 
 const SignedOut = () => {
-    
+    // const MILLISECOND_TOLERANCE = 20;
+    let lastClick = Date.now();
     const [userSettingsMenuOpen, setUserSettingsMenuOpen] = useState(false);
-    const toggle = () => {
+    // const openMenu = (ev) => {
+    //     if (Date.now() - lastClick > MILLISECOND_TOLERANCE) {
+    //         setUserSettingsMenuOpen(true);
+    //     }
+    // }
+    const toggle = (ev) => {
         setUserSettingsMenuOpen(!userSettingsMenuOpen);
     }
 
+    useEffect(() => {
+        const closeMenu = (ev) => {
+            setUserSettingsMenuOpen(false);
+            lastClick = Date.now();
+        }
+
+        document.querySelector('body').addEventListener('click', closeMenu);
+        // return () => {
+        //     document.querySelector('body').removeEventListener('click', closeMenu);
+        // };
+    }, [])
+
     return (
         <div className="profile signed-out">
+        {/* <img onClick={openMenu} src={profileButton} alt="User Menu" /> */}
         <img onClick={toggle} src={profileButton} alt="User Menu" />
         {userSettingsMenuOpen ?
             <nav>
